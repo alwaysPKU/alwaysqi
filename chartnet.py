8be996c6e2c406c094f872ec58676e9649d053d4
alwaysqi <zhangwei@MacBook-Pro.local>
2016-10-27 19:56:45 +0800

    restart

--- chartnet.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*- 
-
-from chartnet import app
-#app.run(port=80)
-
-if __name__ == "__main__":
-	app.run()
--- index.wsgi
+++
@@ -1,5 +0,0 @@
-import sae
-
-from chartnet import app
-
-application = sae.create_wsgi_app(app)
