367db564e6fcaed0c15d457a34477ac67e64ab87
alwaysqi <zhangwei@MacBook-Pro.local>
2016-10-27 20:48:47 +0800

    django1,2,7

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,24 +1,18 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^mysite/', include('mysite.foo.urls')),
-    url(r'^polls/', include('polls.urls')),
+    # Example:
+    # (r'^mysite/', include('mysite.foo.urls')),
+    (r'^$', 'mysite.views.hello'),
+    (r'^demo/$', 'mysite.demo.views.showdemo'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
-
-# Serve static files for admin, use this for debug usage only
-# `python manage.py collectstatic` is preferred.
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-urlpatterns += staticfiles_urlpatterns()
-
